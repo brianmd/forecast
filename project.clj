789682563351cc09c600a8666b23eaaab6ca6dbe
Brian Murphy-Dye <brian@murphydye.com>
2017-02-07 18:22:00 -0700

    Initial commit.

---
+++ project.clj
@@ -0,0 +1,13 @@
+(defproject forecast "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.9.0-alpha14"]
+                 [org.clojure/core.async "0.2.395"]
+                 [clj-http "2.3.0"]
+                 [incanter "1.5.7"]
+                 ]
+  :main ^:skip-aot forecast.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all}})
