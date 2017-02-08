e066e93e2bb80736285325590e8e54a2d3782f35
Brian Murphy-Dye <brian@murphydye.com>
2017-02-07 19:18:44 -0700

    Add channel repository code.

--- src/forecast/repository/channel.clj
+++ src/forecast/repository/channel.clj
@@ -1,15 +1,26 @@
 (ns forecast.repository.channel
-  )
+  (:require [clojure.core.async
+             :as a
+             :refer [>! <! >!! <!! go chan buffer close! thread
+                     alts! alts!! timeout]]
+            ))
+
+(def ip-chan (chan))
+(def location-chan (chan))
 
 (defn put-ip
-  [ip])
+  [ip]
+  (>! ip-chan ip))
 
 (defn get-ip
-  [])
+  []
+  (<! ip-chan))
 
 (defn put-location
-  [location])
+  [location]
+  (>! location-chan location))
 
 (defn get-location
-  [])
+  []
+  (<! location-chan))
 
