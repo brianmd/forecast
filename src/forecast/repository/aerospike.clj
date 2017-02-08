c9dca8dace38e8145636a2df535aeaa942e5873e
Brian Murphy-Dye <brian@murphydye.com>
2017-02-07 19:13:50 -0700

    Rename resolver to repository.

---
+++ src/forecast/repository/aerospike.clj
@@ -0,0 +1,14 @@
+(ns forecast.repository.aerospike
+  )
+
+(defn put-ip
+  [ip])
+
+(defn get-ip
+  [])
+
+(defn put-location
+  [location])
+
+(defn get-location
+  [])
---
+++ src/forecast/repository/channel.clj
@@ -0,0 +1,15 @@
+(ns forecast.repository.channel
+  )
+
+(defn put-ip
+  [ip])
+
+(defn get-ip
+  [])
+
+(defn put-location
+  [location])
+
+(defn get-location
+  [])
+
