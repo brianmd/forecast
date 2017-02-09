c433b6e8a522ede7ee3d8e9f145d60bad40d28af
Brian Murphy-Dye <brian@murphydye.com>
2017-02-08 19:29:45 -0700

    Inform how to sign up for weather api in error message.

--- src/forecast/repository/location_forecast.clj
+++ src/forecast/repository/location_forecast.clj
@@ -1,24 +1,26 @@
 (ns forecast.repository.location-forecast
   (:require [clj-http.client :as client]
             [cheshire.core :refer [parse-string]]
-            [clojure.string :refer [split]]
             [clojure.walk :refer [keywordize-keys]]
             )
   (:import [java.util Calendar]))
+
+
+
 
 (defonce locations (atom {}))
 (defn clear-locations [] (reset! locations {}))
 
 (defn api
   []
-  (or (System/getenv "WEATHER_API") (throw "Must set WEATHER_API envrionment variable")))
+  (or (System/getenv "WEATHER_API") (throw "Must set WEATHER_API envrionment variable. Sign up at http://www.openweathermap.com/.")))
 
 (defn tomorrow
+  "return tomorrow's date as a string in yyyy-mm-dd format"
   []
   (.format (java.text.SimpleDateFormat. "yyyy-MM-dd")
            (java.util.Date. (+ (* 1 86400 1000) (.getTime (java.util.Date.))))
            ))
-;; (tomorrow)
 
 (defn force-location->forecast
   [location]
@@ -38,8 +40,6 @@
         {:error (str "error response for location (" (:status response) ")")}
         ))
     (catch Exception e {:error (str e)})))
-;; (prn (force-location->forecast {:latitude 37.386 :longitude -122.0838}))
-;; (prn (location->forecast {:latitude 37.386 :longitude -122.0838}))
 
 (defn location->forecast
   [location]
