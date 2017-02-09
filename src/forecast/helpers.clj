367aa39e867552a590db11631b2837879d500541
Brian Murphy-Dye <brian@murphydye.com>
2017-02-08 19:25:12 -0700

    Add histogram function.
    Incanter is based on R, but omitted the ability to
    get histogram data -- only a chart was provided out
    of the box.

--- src/forecast/helpers.clj
+++ src/forecast/helpers.clj
@@ -1,4 +1,5 @@
-(ns forecast.helpers)
+(ns forecast.helpers
+  (:import [org.jfree.data.statistics HistogramDataset]))
 
 (def ip-regex
   #"^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$")
@@ -7,6 +8,15 @@
   [ip]
   (not (nil? (re-matches ip-regex ip))))
 
+(defn histogram
+  [data num-bins]
+  (let [hist (HistogramDataset.)]
+    (.addSeries hist "x" (double-array data) num-bins)
+    (map #(vector (.getStartX hist 0 %) (.getEndX hist 0 %) (.getY hist 0 %)) (range (.getItemCount hist 0)))
+    ))
+;; (histogram [1 7 22 4 2 19] 5)
+
 (defn log-error
   [msg value]
   (println msg value))
+
