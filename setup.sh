249b85e2c402d958624721d6dbf431d6ce000125
Brian Murphy-Dye <brian@murphydye.com>
2017-02-19 15:02:20 -0700

    Improve version regex check.

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 . ./version.sh
 
-if ! ( docker images | grep "forecast.*$FORECAST_VERSION" >/dev/null ); then
+if ! ( docker images | grep "forecast\\s*${FORECAST_VERSION}\\s" >/dev/null ); then
     echo "build forecast docker image $FORECAST_VERSION"
     ./build.sh
 fi
